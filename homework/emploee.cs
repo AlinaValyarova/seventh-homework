285c8075e43b9255f89c393a4f626df2af1ddb33
AlinaValyarova <113130561+AlinaValyarova@users.noreply.github.com>
2022-11-04 22:39:31 +0300

    Добавьте файлы проекта.

---
+++ homework/emploee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace homework
+{
+    class emploee
+    {
+        public string name;
+        public string position;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+    }
+
+
+}
---
+++ tumakov 2.0/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace tumakov_2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ex1();
+        }
+
+        public class account
+        {
+            public int num;
+            public decimal money;
+            public string Initials;
+        }
+        public static void AddMoney(List<account> bank)
+        {
+            Console.WriteLine("Enter number of your account");
+            int acnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Emter amount of money you want to add");
+            decimal num = Convert.ToDecimal(Console.ReadLine());
+            bank[acnum].money += num;
+            Console.WriteLine("money were added successfully");
+
+        }
+
+        public static void TakeMoney(List<account> bank)
+        {
+            Console.WriteLine("Enter number of your account");
+            int acnum = Convert.ToInt32(Console.ReadLine());
+            var found = bank.Find(p => p.num == acnum);
+
+            Console.WriteLine("Emter amount of money you want to take");
+            decimal num = Convert.ToDecimal(Console.ReadLine());
+            int a = bank.IndexOf(found);
+            if (bank[a].money >= num)
+            {
+                bank[a].money -= num;
+                Console.WriteLine("success!");
+            }
+            else
+            {
+                Console.WriteLine("You don't have enough money");
+            }
+
+
+
+        }
+
+        public static void NumOfAcc(int account)
+        {
+            account newclient = new account();
+            account += 1;
+            account = newclient.num;
+        }
+
+        public static void ex1()
+        {
+            Console.WriteLine("В класс банковский счет, созданный в упражнениях 7.1- 7.3 добавить метод, который переводит деньги с одного счета на другой." +
+                "У метода два параметра: ссылка на объект класса банковский счет откуда снимаются деньги, второй параметр – сумма.");
+            List<account> clients = new List<account>();
+            int count = 0;
+            Console.WriteLine("Choose needed operation");
+            Console.WriteLine("1 - Add new client");
+            Console.WriteLine("2 - Find account information");
+            Console.WriteLine("3 - Deposit money");
+            Console.WriteLine("4 - Withdraw money");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        account newclient = new account();
+                        NumOfAcc(count);
+                        newclient.num = count;
+                        Console.WriteLine("Your number of account: {0}", count);
+                        Console.WriteLine("Enter the account balance");
+                        int sum = Convert.ToInt32(Console.ReadLine());
+                        newclient.money = sum;
+                        Console.WriteLine("Enter Initials of the client");
+                        string name = Console.ReadLine();
+                        newclient.Initials = name;
+                        clients.Add(newclient);
+                        return;
+                    case 2:
+                        AddMoney(clients);
+                        return;
+
+                    case 3:
+                        TakeMoney(clients);
+                        return;
+
+                    default:
+                        Console.WriteLine("You entered something wrong");
+                        return;
+                }
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+}
